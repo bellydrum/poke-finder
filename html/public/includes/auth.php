8993d2ebb4a33f259ec0aaf22c1f5460ca5ced79
david <davidmaness1990@gmail.com>
2016-09-27 20:08:50 -0500

    fix auth file i guess

---
+++ html/public/includes/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+	if(getenv("PHP_ENV") == "production")
+	{
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));		
+
+		$host = $url["host"];
+		$user = $url["user"];
+		$pass = $url["pass"];
+		$name = substr($url["path"], 1);
+	}
+	else
+	{
+		$host = 'localhost';
+		$user = 'b3llydrum';
+		$pass = 'Bittie90';
+		$name = 'pokedex';
+	}
+
+	$db = new PDO('mysql:host=localhost;dbname=pokedex', $user, $pass);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// for debugging
+	// echo 'Connected successfully!';
+
+
+?>
