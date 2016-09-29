d0a5202ec34ad3488a885c389ae6f61b01b0b1ee
david <davidmaness1990@gmail.com>
2016-09-29 10:08:28 -0500

    finish basic about page; add pokemon logo link

--- public/views/pokemon_logo.php
+++ public/views/pokemon_logo.php
@@ -1,3 +1,5 @@
 <div id='pokemon-header-wrapper'>
-	<img src='includes/images/pokemon_logo.png' id='pokemon-header'/>
+	<a href='index.php'>
+		<img src='includes/images/pokemon_logo.png' id='pokemon-header'/>
+	</a>
 </div>
