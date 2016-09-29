9e8dc48b36c5a34a9d7f888a4ecb869d21469acd
david <davidmaness1990@gmail.com>
2016-09-28 19:11:43 -0500

    edit about page

--- public/about.php
+++ public/about.php
@@ -13,7 +13,7 @@
 	// insert pokemon logo
 	render('pokemon_logo');
 
-	echo "Hello, world!\n";
+	echo "<div id='about'>Poke-Finder is your helper for keeping track of your National Dex. This page is under construction. This page will have a list of functionality as well as plans for the future. Message me on reddit at /u/b3llydrum if you want to contact me!</div>";
 
 
 	render('footer');
