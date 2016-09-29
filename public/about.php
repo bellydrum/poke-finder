5c714df994d8dac639942acada12b7b362bd5de5
david <davidmaness1990@gmail.com>
2016-09-29 09:58:44 -0500

    add to about page

--- public/about.php
+++ public/about.php
@@ -13,8 +13,9 @@
 	// insert pokemon logo
 	render('pokemon_logo');
 
-	echo "<div id='about'>Poke-Finder is your helper for keeping track of your National Dex. This page is under construction. This page will have a list of functionality as well as plans for the future. Message me on reddit at /u/b3llydrum if you want to contact me!</div>";
-
+	echo "<div id='about' class='wireframe-1'><p>Poke-Finder is your helper for keeping track of your National Dex.</p><p>This site is under construction.</p><p>This page will have a list of functionality as well as plans for the future.</p><p>Message me on reddit at /u/b3llydrum if you want to contact me!</p></div>";
+	echo "<div id='about-2' class='wireframe-1'>Here is how to use Poke-finder:</div>";
+	echo "<table id='about-table' class='wireframe-1'><tr class='wireframe-2'><td class='wireframe-3'>Search for a Pokemon using the search parameters</td></tr><tr class='wireframe-2'><td class='wireframe-3'>Click the Pokemon to see Bulbapedia's entry on how to catch it</td></tr><tr class='wireframe-2'><td class='wireframe-3'>Click the Pokeball next to the Pokemon once you've caught it</td></tr></table>";
 
 	render('footer');
 ?>
--- public/views/header.php
+++ public/views/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset='utf-8'>
 		<title><?=htmlspecialchars($title)?></title>
-		<link href='../stylesheets/stylesheet.css' rel='stylesheet'/>
+		<link href='stylesheets/stylesheet.css' rel='stylesheet'/>
 		<link rel='shortcut icon' type='image/x-icon' href='/var/www/html/includes/images/header_icons/photo_album.ico'/>
 		<!--script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script-->
 		<script src="https://code.jquery.com/jquery-latest.js"></script>
